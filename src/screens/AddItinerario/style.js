fe1b7cfbface69fcc584b58b3b3cb2a1ca64f02f
guinaoki <rm551456@fiap.com.br>
2024-09-17 11:39:57 -0300

    arrumando erros

--- src/screens/AddItinerario/style.js
+++ src/screens/AddItinerario/style.js
@@ -44,6 +44,64 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#666',
   },
+  formContainer: {
+    backgroundColor: '#fff', // Fundo branco para o formulário
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#eca406', // Borda laranja para o formulário
+    padding: 16,
+    marginBottom: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#eca406', // Borda laranja para campos de entrada
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 8,
+    fontSize: 16,
+    color: '#333', // Cor do texto para boa legibilidade
+  },
+  button: {
+    backgroundColor: '#eca406', // Cor laranja para botões
+    padding: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  selectButton: {
+    borderWidth: 1,
+    borderColor: '#eca406', // Borda laranja para botões de seleção
+    backgroundColor: '#fff', // Fundo branco para contraste
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  selectButtonText: {
+    color: '#eca406', // Texto laranja para botões de seleção
+    fontSize: 16,
+  },
+  focusOption: {
+    borderWidth: 1,
+    borderColor: '##eca406', // Borda laranja para opções de foco
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 8,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  focusOptionText: {
+    color: '#eca406', // Texto laranja para opções de foco
+    fontSize: 16,
+  }
 });
 
 export default styles;
